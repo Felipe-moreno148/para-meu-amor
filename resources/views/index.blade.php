e0fbfab662e029724d37a9b084e2e4444d9fd61f
felipe <felipemoreno148@gmail.com>
2025-06-08 13:45:24 -0300

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Para meu amor</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <div class="min-h-screen bg-slate-800">
+        <audio autoplay loop>
+            <source src="musica.mp3" type="audio/mpeg">
+        </audio>
+    </div>
+</body>
+
+</html>
