bae0b2bd8389d73150936914cf82ba74e2210c81
felipe <felipemoreno148@gmail.com>
2025-06-10 18:13:44 -0300

    commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,33 +14,41 @@
 </head>
 
 
-<body class="flex flex-col items-center justify-center min-h-screen text-center px-4">
+<body class="flex flex-col items-center justify-center w-full text-center px-4">
 
-<h1 class="text-slate-800 text-3xl">Feliz dia dos namorados!!!</h1>
-<p>toque no coração</p>
-<button onclick="toggleMusic()" id="playButton">
-    <img src="{{ asset('heart.png') }}" alt="coração" class="max-h-20">
-</button>
+    <h1 class="text-slate-800 text-3xl">Feliz dia dos namorados!!!</h1>
+    <p>toque no coração</p>
+    <button onclick="toggleMusic()" id="playButton" class="m-4">
+        <img src="{{ asset('heart.png') }}" alt="coração" class="max-h-20">
+    </button>
 
-<!-- Áudio -->
-<audio id="musica" loop>
-    <source src="{{ asset('planos.mp3') }}" type="audio/mpeg">
-    Seu navegador não suporta áudio HTML5.
-</audio>
+        <img src="{{ asset('amorzinho/foto23.JPG') }}" alt="foto nossa"
+        class="max-h-[380px] border-4 border-purple-500 border-b-4 rounded-2xl m-4">
+        <p class="text-sm max-w-[286px]">
+            Adoro esta foto, ela é espontânea e mostra como você me olha como se eu fosse
+            algo incrivel, essa foto é a prova viva de que você me ama
+        </p>
 
-<script>
-    const audio = document.getElementById("musica");
+        <img src="{{ asset('assets/ena-joel-g.gif') }}" alt="">
+    <!-- Áudio -->
+    <audio id="musica" loop>
+        <source src="{{ asset('planos.mp3') }}" type="audio/mpeg">
+        Seu navegador não suporta áudio HTML5.
+    </audio>
 
-    function toggleMusic() {
-        if (audio.paused) {
-            audio.play();
-            console.log('play');
-        } else {
-            audio.pause();
-            console.log('pause');
+    <script>
+        const audio = document.getElementById("musica");
+
+        function toggleMusic() {
+            if (audio.paused) {
+                audio.play();
+                console.log('play');
+            } else {
+                audio.pause();
+                console.log('pause');
+            }
         }
-    }
-</script>
+    </script>
 
 </body>
 
