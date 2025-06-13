58c669860cd9c7f1d4a8e5c4d0a1880b9891758f
felipe <felipemoreno148@gmail.com>
2025-06-12 21:55:58 -0300

    FUCK

--- public/index.php
+++ public/index.php
@@ -1,31 +1,20 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-$app = Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        // Registre middlewares se quiser
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        // Registre handlers de exceções se quiser
-    })
-    ->create();
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+$app->handleRequest(Request::capture());
